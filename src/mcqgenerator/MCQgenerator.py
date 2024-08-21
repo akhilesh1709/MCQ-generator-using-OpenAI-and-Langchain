b5664c724edcdeedfdd43489c06d06329bdb4ef8
Akhilesh <103525428+akhilesh1709@users.noreply.github.com>
2024-08-21 21:39:06 +0530

    Changes to Streamlit API key

--- src/mcqgenerator/MCQgenerator.py
+++ src/mcqgenerator/MCQgenerator.py
@@ -5,6 +5,7 @@ import pandas as pd
 from dotenv import load_dotenv
 from src.mcqgenerator.utils import read_file, get_table_data
 from src.mcqgenerator.logger import logging
+import streamlit as st
 
 # Importing necessary packages from langchain
 from langchain.chat_models import ChatOpenAI
@@ -15,7 +16,7 @@ from langchain.chains import LLMChain, SequentialChain
 load_dotenv()
 
 # Access the environment variables
-openai_api_key = os.getenv("OPENAI_API_KEY")
+openai_api_key = st.secrets.get("OPENAI_API_KEY")
 
 if not openai_api_key:
     raise ValueError("OpenAI API key is not set. Please check your .env file.")
