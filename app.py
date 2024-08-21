8f105b2e7a693507b61cee4eadaea871ef5ca8be
Akhilesh <103525428+akhilesh1709@users.noreply.github.com>
2024-08-21 21:34:14 +0530

    Added final changes

--- app.py
+++ app.py
@@ -4,24 +4,24 @@ import traceback
 import pandas as pd
 from dotenv import load_dotenv
 from src.mcqgenerator.utils import read_file, get_table_data
-import streamlit as st  
+import streamlit as st
 from langchain.callbacks import get_openai_callback
 from src.mcqgenerator.MCQgenerator import generate_evaluate_chain
 from src.mcqgenerator.logger import logging
 
-with open('response.json','r') as file:
+with open('response.json', 'r') as file:
     RESPONSE_JSON = json.load(file)
 
-st.title("MCQs creator application with langchain")
+st.title("MCQs creator")
 
 with st.form("user_inputs"):
     uploaded_file = st.file_uploader("Upload a PDF or txt file ")
 
-    mcq_count = st.number_input("No. of MCQs",min_value=3, max_value=50)
+    mcq_count = st.number_input("No. of MCQs", min_value=3, max_value=50)
 
-    subject = st.text_input("Inser subject",max_chars=20)
+    subject = st.text_input("Insert subject", max_chars=20)
 
-    tone = st.text_input("Complexity level of questions",max_chars=20, placeholder="Simple")
+    tone = st.text_input("Complexity level of questions", max_chars=20, placeholder="Simple")
 
     button = st.form_submit_button("Create MCQs")
 
@@ -30,18 +30,16 @@ with st.form("user_inputs"):
             try:
                 text = read_file(uploaded_file)
                 with get_openai_callback() as cb:
-                    response=generate_evaluate_chain({
-                        "text":text,
+                    response = generate_evaluate_chain({
+                        "text": text,
                         "number": mcq_count,
                         "subject": subject,
-                        "tone":tone,
+                        "tone": tone,
                         "response_json": json.dumps(RESPONSE_JSON)
-                        }
-                    )
+                    })
             except Exception as e:
                 traceback.print_exception(type(e), e, e.__traceback__)
                 st.error(e)
-            
             else:
                 print(f"Total tokens:{cb.total_tokens}")
                 print(f"Prompt tokens:{cb.prompt_tokens}")
@@ -54,11 +52,9 @@ with st.form("user_inputs"):
                         table_data = get_table_data(quiz)
                         if table_data is not None:
                             df = pd.DataFrame(table_data)
-                            df.index = df.index+1
+                            df.index = df.index + 1
                             st.table(df)
-                            st.text_area(label="Review",value=response["review"])
                         else:
                             st.error("Failed to generate MCQs")
-                
                 else:
                     st.write(response)
