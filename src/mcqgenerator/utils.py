4edfa9f0e9dc90efe76dff7dcf454bb41f5728d1
Akhilesh <103525428+akhilesh1709@users.noreply.github.com>
2024-08-21 21:43:21 +0530

    Changed UI

--- src/mcqgenerator/utils.py
+++ src/mcqgenerator/utils.py
@@ -27,14 +27,14 @@ def read_file(file):
 
 def get_table_data(quiz_str):
     try:
-        # convert the quiz from a str to dict
+        # Convert the quiz from a str to dict
         quiz_dict = json.loads(quiz_str)
         quiz_table_data = []
 
-        # iterate over the quiz dictionary and extract the required information
+        # Iterate over the quiz dictionary and extract the required information
         for key, value in quiz_dict.items():
             mcq = value["mcq"]
-            options = " || ".join([f"{option}-> {option_value}" for option, option_value in value["options"].items()])
+            options = "\n".join([f"{option}: {option_value}" for option, option_value in value["options"].items()])
             correct = value["correct"]
             quiz_table_data.append({"MCQ": mcq, "Choices": options, "Correct": correct})
 
@@ -47,9 +47,5 @@ def get_table_data(quiz_str):
     
     except Exception as e:
         logging.error(f"Unexpected error in get_table_data: {e}")
-        traceback.print_exception(type(e), e, e.__traceback__)
-        return False
-    
-    except Exception as e:
         traceback.print_exception(type(e), e, e.__traceback__)
         return False
